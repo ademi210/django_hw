988a0b5efa3d4825e4a44e54bd991f0e9a734b40
ademi210 <aaskerbekova161@gmail.com>
2025-03-13 16:24:07 +0600

    homework2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    GENRE = (
+        ('Детектив', 'Детектив'),
+        ('Мистика', 'Мистика'),
+    )
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.FloatField
+    created_at = models.DateTimeField(auto_now_add=True)
+    genre = models.CharField(max_length=10, choices=GENRE)
+    author_email = models.EmailField()
+    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='book/')
+    video_url = models.URLField()
+
+    def __str__(self):
+        return self.title
+
--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
+    path('book_list/', views.books_list),
     path('about_me/', views.about_me),
     path('favorite_animal/', views.favorite_animal),
     path('system_time/', views.system_time),
+    path('book_list/<int:id>/', views.book_detail),
     ]
